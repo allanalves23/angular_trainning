b417fa639a9692eb25a59bcafe7ca1bc9a79c932
Allan Wanderley <allan.alves@wg2r.com>
2021-09-16 19:02:17 -0300

    chore: treinamento #01 - ngModel, click e *ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public nome = "";
+  public deveMostrarNome = false;
   title = 'ngModel';
+
+  public apresentarNome() {
+    this.deveMostrarNome = true;
+  }
 }
